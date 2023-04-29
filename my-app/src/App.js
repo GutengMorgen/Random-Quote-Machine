e8d59152ff9937c07eb323855b31c8ea732b290d
guten-morgen <crisstian2345@gmail.com>
2023-04-29 18:51:35 -0500

    agrega funcion para imprimir en consola el texto del textarea

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,90 @@
 import React, {Component} from 'react';
-import './styles.sass'
+import PropTypes from 'prop-types';
+import './styles.css'
 
-function Mynigger()
+function TwitterBt()
 {
   return (
-    <button>Hola</button>
+    <button>tweet</button>
   );
 }
 
+function TumblerBt()
+{
+  return (
+    <button>tumbler</button>
+  );
+}
 
+//principal button
+function QuoteBt({text, setColor, color})
+{
+  const handleClick = () =>
+  {
+    console.log(text);
+    setColor(color === 'blue' ? 'red' : 'blue');
+  }
+
+  return (
+    <button style={{backgroundColor: color}} onClick={handleClick}>
+      quote
+    </button>
+  );
+}
+
+//textarea
+function Mytext({onChange, value})
+{
+  const handleOnChange = (event) =>
+  {
+    onChange(event.target.value);
+  }
+
+  return (
+    <textarea value={value} onChange={handleOnChange}></textarea>
+  )
+}
+
+//proptypes
+QuoteBt.prototype = {
+  text: PropTypes.string.isRequired,
+  setColor: PropTypes.func.isRequired,
+  color: PropTypes.string.isRequired
+}
+Mytext.prototype = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired
+}
+
+//class default App
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      text: '',
+      color: 'red'
+    }
+  }
+
+  handleTextChange = (text) => {
+    this.setState({text});
+  }
+
+  handleSetColor = (color) => {
+    this.setState({color})
+  }
+
   render(){
+    const {text, color} = this.state;
+
     return(
-      <div class='mydiv'>
-        <Mynigger/>
+      <div className='mydiv'>
+        <p>
+          <Mytext onChange={this.handleTextChange} value={text}/>
+        </p>
+        <TwitterBt/>
+        <TumblerBt/>
+        <QuoteBt text={text} setColor={this.handleSetColor} color={color}/>
       </div>
     )
   }
